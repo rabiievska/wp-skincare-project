f27e7546da81557a7df066c5a77a76efee301279
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-05-10 18:31:47 +0200

    Added archive-product page, added header and footer

--- front-page.php
+++ front-page.php
@@ -1,48 +1,5 @@
-<div class="index-page-width">
-  <h1>Here is a list of my favorite skincare products</h1>
+<?php get_header(); ?>
 
-  <div class="skincare-list-wrapper">
-    <?php 
-      $skincareProducts = new WP_Query(array(
-        'posts_per_page' => 18,
-        'post_type' => 'Product'
-      ));
+<p><a href="<?php echo get_post_type_archive_link('product'); ?>" class="">View My Favorite Products</a></p>
 
-      while($skincareProducts->have_posts()) {
-        $skincareProducts->the_post(); ?>
-        <div class="product-item">
-          <a href="<?php the_permalink(); ?>">
-            <div class="product-item-image">
-              <?php the_post_thumbnail(); ?>
-            </div>
-            <div class="product-info-wrapper">
-              <div class="product-title">
-                <?php the_title(); ?>
-              </div>
-              <div class="product-review">
-                <?php 
-                  $review = the_field('review'); 
-                  echo $review;
-                ?>
-              </div>
-              <div class="product-brandname">
-              <?php 
-                $brandName = the_field('brand_name'); 
-                echo $brandName; 
-              ?>
-              </div>
-              <p class="product-description">
-                <?php 
-                  echo wp_trim_words(get_the_content(), 18)
-                ?>
-                <span class="learn-more">
-                  Learn more
-                </span>
-              </p>
-            </div>
-          </a>
-        </div>
-      <?php }
-    ?>
-  </div>
-</div>
+<?php get_footer(); ?>
