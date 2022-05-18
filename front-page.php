3349aae902eaf07dcd624e00fe28c2265d05607f
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-05-18 21:26:00 +0200

    Frontpage styling is finished

--- front-page.php
+++ front-page.php
@@ -3,52 +3,62 @@
 <div class="frontpage">
   <div class="frontpage-page-width">
 
-    <div class="frontpage-about-me">
-      <img class="avatar" src="<?php echo get_theme_file_uri( 'assets/avatar.jpg' ); ?>" alt="avatar" />
-      <p>Hi there!</p>
-      <p>My name is Tania, I am 33 years old and I am skincare addicted.</p>
-      <p>I have a combination, akne-prone skin with a lot of dark freckles. During whole my life my skin was dehydrated and dull, with huge clogged pores and extreme oiliness 
-        in the T-zone. It had constant breakouts, lack of elasticity, noticeable wrinkles between the eyebrows, flaking, a feeling of dryness and tightness.</p>
-      <p>At the time, I knew nothing about skincare. For a long time, the only products I used were makeup remover milk and cleansing gel. After 25, 
-        I bought my first full skincare, which included toner, day and night cream, and eye cream. </p>
-      <p>I chose cheap and low-quality cosmetics with strong perfume, which made me gasp. Such experiments did not bring any results and after a year or so I 
-        switched to professional cosmetics.</p>
-      <p>My care consisted then of a salicylic acid cleansing gel, a restorative night cream, and a light cream fluid, which I used as a day cream.</p>
-      <p>Using these products greatly improved the condition of my skin, although it did not solve the problems I had.</p>
-      <p>After I moved to Berlin in 2016, the condition of my skin deteriorated significantly. It was getting worse every day. At that time I continued to use Ukrainian 
-        professional care cosmetics, started taking vitamins and supplements designed for the skin. But nothing helped.</p>
-      <p>Around this time period I learned about AHA and BHA acids from my friend. Then I bought Bioderma Sébium
-        Night Peel and was amazed by the results. Pores narrowed and cleared up, breakouts became very rare, and my skin tone improved.</p>
-      <p>It became a point of reference. And I slowly began to learn about skincare, the ingredients, their combinations, what's useful and what's best to avoid.</p>
-      <p>Since then, I have read hundreds of articles and studies, tested about a thousand different care products and identified my favorite brands and products and 
-        deduced the perfect formula for me.</p>
-      <p>Thanks to this I have been able to improve my skin significantly. Of course, it is still far from perfect, but I don't give up and keep going.</p>
+    <div class="frontpage-content">
+      <section class="frontpage-about-me">
+        <img class="avatar" src="<?php echo get_theme_file_uri( 'assets/avatar.jpg' ); ?>" alt="avatar" />
+        <p><i class="first-letter">&#8459;</i>i there!</p>
+        <p>My name is Tania, I am 33 years old and I am skincare addicted.</p>
+        <p>I have a combination, akne-prone skin with a lot of dark freckles. During whole my life my skin was dehydrated and dull, with huge clogged pores and extreme oiliness 
+          in the T-zone. It had constant breakouts, lack of elasticity, noticeable wrinkles between the eyebrows, flaking, a feeling of dryness and tightness.</p>
+        <p>At the time, I knew nothing about skincare. For a long time, the only products I used were makeup remover milk and cleansing gel. After 25, 
+          I bought my first full skincare, which included toner, day and night cream, and eye cream. </p>
+        <p>I chose cheap and low-quality cosmetics with strong perfume, which made me gasp. Such experiments did not bring any results and after a year or so I 
+          switched to professional cosmetics.</p>
+        <p>My care consisted then of a salicylic acid cleansing gel, a restorative night cream, and a light cream fluid, which I used as a day cream.</p>
+        <p>Using these products greatly improved the condition of my skin, although it did not solve the problems I had.</p>
+        <p>After I moved to Berlin in 2016, the condition of my skin deteriorated significantly. It was getting worse every day. At that time I continued to use Ukrainian 
+          professional care cosmetics, started taking vitamins and supplements designed for the skin. But nothing helped.</p>
+        <p>Around this time period I learned about AHA and BHA acids from my friend. Then I bought Bioderma Sébium
+          Night Peel and was amazed by the results. Pores narrowed and cleared up, breakouts became very rare, and my skin tone improved.</p>
+        <p>It became a point of reference. And I slowly began to learn about skincare, the ingredients, their combinations, what's useful and what's best to avoid.</p>
+        <p>Since then, I have read hundreds of articles and studies, tested about a thousand different care products and identified my favorite brands and products and 
+          deduced the perfect formula for me.</p>
+        <p>Thanks to this I have been able to improve my skin significantly. Of course, it is still far from perfect, but I don't give up and keep going.</p>
+      </section>
+      <section class="frontpage-destinations">
+        <h2>What has improved so far:</h2>
+        <ul>
+          <li><span>&#10004;</span>Freckles became very light and barely noticeable</li>
+          <li><span>&#10004;</span>Breakouts are very rare. Mostly they appear in hot weather and after eating unhealthy or sugary foods</li>
+          <li><span>&#10004;</span>My skin became evenly and beautifully colored and had a healthy glow</li>
+          <li><span>&#10004;</span>The pores in the T-zone have narrowed and cleared up. If before, after a professional cosmetic facial cleansing, 
+            my pores would become completely clogged after three days, now they remain clear for 2 to 4 weeks.</li>
+          <li><span>&#10004;</span>Improved skin elasticity and firmness</li>
+          <li><span>&#10004;</span>Thin, shallow wrinkles smoothed out or disappeared completely, and deeper ones stopped progressing</li>
+          <li><span>&#10004;</span>The skin type is given by nature and cannot be changed, but among the improvements I can note that the sebum secretion 
+            in the T-zone has slightly decreased and its quality has improved. While previously sebum was thick and sticky and felt very 
+            heavy on the skin, now its texture is thinner, lighter and more fluid.</li>
+        </ul>
+      </section>
+      <section class="frontpage-ingredients">
+        <h2>Favorite ingredients:</h2>
+        <ul>
+          <li><span>&#10086;</span><b>Winter: </b>Retinol + Peptides</li>
+          <li><span>&#10086;</span><b>Autumn, spring: </b>AHA + Vitamin C + Peptides</li>
+          <li><span>&#10086;</span><b>Summer: </b>BHA + Vitamin C + Peptides + Niacinamide</li>
+        </ul>
+      </section>
+      <section class="frontpage-brands">
+        <h2>Favorite Skin Care Cosmetics Brands:</h2>
+        <ul>
+          <li><span>&#10140;</span><b>Korean: </b>COSRX, Pyunkang Yul, Benton, Mizon, Purito, Dear Klairs</li>
+          <li><span>&#10140;</span><b>USA: </b>Paula's Choice</li>
+          <li><span>&#10140;</span><b>Europe: </b>Bioderma, The Inkey List, The Ordinary, CeRaVe, Balea Med, Isana Med, Isana Pure, Balea Beauty Expert</li>
+        </ul>
+      </section>
+      
+      <p class="frontpage-link-wrap"><a href="<?php echo get_post_type_archive_link('product'); ?>" class="">View My Favorite Products</a></p>
     </div>
-    <div class="frontpage-destinations">
-      <h2>What has improved so far:</h2>
-      <ul>
-        <li>Freckles became very light and barely noticeable</li>
-        <li>Breakouts are very rare. Mostly they appear in hot weather and after eating unhealthy or sugary foods</li>
-        <li>My skin became evenly and beautifully colored and had a healthy glow</li>
-        <li>The pores in the T-zone have narrowed and cleared up. If before, after a professional cosmetic facial cleansing, 
-          my pores would become completely clogged after three days, now they remain clear for 2 to 4 weeks.</li>
-        <li>Improved skin elasticity and firmness</li>
-        <li>Thin, shallow wrinkles smoothed out or disappeared completely, and deeper ones stopped progressing</li>
-        <li>The skin type is given by nature and cannot be changed, but among the improvements I can note that the sebum secretion 
-          in the T-zone has slightly decreased and its quality has improved. While previously sebum was thick and sticky and felt very 
-          heavy on the skin, now its texture is thinner, lighter and more fluid.</li>
-      </ul>
-    </div>
-    <div class="frontpage-brands">
-      <h2>Favorite Skin Care Cosmetics Brands:</h2>
-      <ul>
-        <li><b>Korean: </b>COSRX, Pyunkang Yul, Benton, Mizon, Purito, Dear Klairs</li>
-        <li><b>USA: </b>Paula's Choice</li>
-        <li><b>Europe: </b>Bioderma, The Inkey List, The Ordinary, CeRaVe, Balea Med, Isana Med, Isana Pure, Balea Beauty Expert</li>
-      </ul>
-    </div>
-    
-    <p><a href="<?php echo get_post_type_archive_link('product'); ?>" class="">View My Favorite Products</a></p>
   </div>
 </div>
 
--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php 
   function skincare_files() {
-    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom-google-fonts-roboto', 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap');
     wp_enqueue_style('skincare_styles', get_theme_file_uri('./style.css'));
   }
   
