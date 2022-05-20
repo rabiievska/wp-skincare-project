c095d08a794b9bf40c6d7931394dc60c01daa1be
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-05-20 10:49:38 +0200

    Add styles for single product page

--- archive-product.php
+++ archive-product.php
@@ -1,55 +1,58 @@
 <?php get_header(); ?>
 
 <div class="archive-page-width">
-  <h1>Here is a list of my favorite skincare products</h1>
+  <div class="archive-content">
+    <h1>Here is a list of my favorite skincare products</h1>
 
-  <div class="archive-skincare-list-wrapper">
-    <?php 
-      $skincareProducts = new WP_Query(array(
-        'posts_per_page' => 18,
-        'post_type' => 'Product'
-      ));
+    <div class="archive-skincare-list-wrapper">
+      <?php 
+        $skincareProducts = new WP_Query(array(
+          'posts_per_page' => 18,
+          'post_type' => 'Product'
+        ));
 
-      while($skincareProducts->have_posts()) {
-        $skincareProducts->the_post(); ?>
-        <div class="archive-product-item">
-          <a href="<?php the_permalink(); ?>">
-            <div class="archive-product-item-image">
-              <?php the_post_thumbnail(); ?>
+        while($skincareProducts->have_posts()) {
+          $skincareProducts->the_post(); ?>
+      <div class="archive-product-item">
+        <a href="<?php the_permalink(); ?>">
+          <div class="archive-product-item-image">
+            <?php the_post_thumbnail(); ?>
+          </div>
+          <div class="archive-product-info-wrapper">
+            <div class="archive-product-title">
+              <?php the_title(); ?>
             </div>
-            <div class="archive-product-info-wrapper">
-              <div class="archive-product-title">
-                <?php the_title(); ?>
-              </div>
-              <div class="archive-product-brandname-review-container">
-                <div class="archive-product-brandname">
+            <div class="archive-product-brandname-review-container">
+              <div class="archive-product-brandname">
                 <?php 
-                  $brandName = the_field('brand_name'); 
-                  echo $brandName; 
-                ?>
-                </div>
-                <div class="archive-product-review">
-                  <?php 
-                    $review = the_field('review'); 
-                    echo $review;
+                      $brandName = the_field('brand_name'); 
+                      echo $brandName; 
+                    ?>
+              </div>
+              <div class="archive-product-review">
+                <?php 
+                      $review = the_field('review'); 
+                      echo $review;
+                    ?>
+              </div>
+            </div>
+            <p class="archive-product-description">
+              <?php 
+                    echo wp_trim_words(get_the_content(), 18)
                   ?>
-                </div>
-              </div>
-              <p class="archive-product-description">
-                <?php 
-                  echo wp_trim_words(get_the_content(), 18)
-                ?>
-              </p>
-              <div class="archive-learn-more-wrapper">
-                <div class="learn-more">
-                  Learn more
-                </div>
+            </p>
+            <div class="archive-learn-more-wrapper">
+              <div class="learn-more">
+                Learn more
               </div>
             </div>
-          </a>
-        </div>
+          </div>
+        </a>
+      </div>
       <?php }
     ?>
+    </div>
+
   </div>
 </div>
 
--- single-product.php
+++ single-product.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
 
 <div class="single-page-width">
+  <div class="single-content">
+    <?php while (have_posts()) {
+        the_post(); ?>
 
-<?php while (have_posts()) {
-    the_post(); ?>
-
-  <h2><?php the_title(); ?></h2>
-  <div class="single-product-item">
-    <div class="single-product-item-image">
-      <?php the_post_thumbnail(); ?>
-    </div>
-    <div class="single-product-info-wrapper">
-      <div class="single-product-brandname-review-container">
-        <div class="single-product-brandname">
-          Brand name:
-        <?php 
-          $brandName = the_field('brand_name'); 
-          echo $brandName; 
-        ?>
-        </div>
-        <div class="single-product-review">
-          My review:
-          <?php 
-            $review = the_field('review'); 
-            echo $review;
-          ?>
+    <h1><?php the_title(); ?></h1>
+    <div class="single-product-item">
+      <div class="single-product-item-image">
+        <?php the_post_thumbnail(); ?>
+      </div>
+      <div class="single-product-info-wrapper">
+        <p class="single-product-description">
+          <?php echo get_the_content(); ?>
+        </p>
+        <div class="single-product-brandname-review-container">
+          <div class="single-product-brandname">
+            <?php 
+              $brandName = the_field('brand_name'); 
+              echo $brandName; 
+            ?>
+          </div>
+          <div class="single-product-review">
+            <?php 
+                $review = the_field('review'); 
+                echo $review;
+              ?>
+          </div>
         </div>
       </div>
-      <p class="single-product-description">
-        <?php echo get_the_content(); ?>
-      </p>
     </div>
   </div>
 </div>
 
 <?php }
+
 
  get_footer(); ?>
